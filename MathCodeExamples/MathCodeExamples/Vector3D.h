1f4e0b7c7c9e15e29cf8e0e25c544e4371fae571
MiguelRodRic <miguelrodric@gmail.com>
2018-04-02 18:27:42 +0200

    Vector Projection and Rejection implemented

--- MathCodeExamples/MathCodeExamples/Vector3D.h
+++ MathCodeExamples/MathCodeExamples/Vector3D.h
@@ -103,3 +103,11 @@ inline Vector3D Cross(const Vector3D& a, const Vector3D& b) {
 		a.z * b.x - a.x * b.z,
 		a.x * b.y - a.y * b.x));
 }
+
+inline Vector3D Project(const Vector3D& a, const Vector3D& b) {
+	return (b * (Dot(a, b) / Dot(b, b)));
+}
+
+inline Vector3D Reject(const Vector3D&a, const Vector3D& b) {
+	return (a - b * (Dot(a, b) / Dot(b, b)));
+}
