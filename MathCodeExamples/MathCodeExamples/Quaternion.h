4de3833a577791d1da04c6d6174c4d7f58851759
MiguelRodRic <miguelrodric@gmail.com>
2019-04-12 17:19:33 +0100

    Remaining basic implementation in Quaternion

--- MathCodeExamples/MathCodeExamples/Quaternion.h
+++ MathCodeExamples/MathCodeExamples/Quaternion.h
@@ -35,10 +35,74 @@ struct Quaternion
 		return (reinterpret_cast<const Vector3D&>(x));
 	}
 
-	Matrix3D GetRotationMatrix(void);
+	Matrix3D GetRotationMatrix(void)
+	{
+		float xsquared = x * x;
+		float ysquared = y * y;
+		float zsquared = z * z;
+		float xy = x * y;
+		float xz = x * z;
+		float yz = y * z;
+		float wx = w * x;
+		float wy = w * y;
+		float wz = w * z;
+		
+		return (Matrix3D(
+			1.0f - 2.0f * (ysquared + zsquared), 2.0f * (xy - wz), 2.0f * (xz + wy),
+			2.0f * (xy + wz), 1.0f - 2.0f * (xsquared + zsquared), 2.0f * (yz - wx),
+			2.0f * (xz + wy), 2.0f * (xy - wz), 1.0f - 2.0f * (xsquared + zsquared)));
+	}
 
-	void SetRotationMatrix(const Matrix3D& m);
+	void SetRotationMatrix(const Matrix3D& m)
+	{
+		float m00 = m(0, 0);
+		float m11 = m(1, 1);
+		float m22 = m(2, 2);
+		float sum = m00 + m11 + m22;
+		
+		if (sum > 0.0f)
+		{
+			w = sqrt(sum + 1.0f) * 0.5f;
+			float f = 0.25f / w;
+			x = (m(2, 1) - m(1, 2)) * f;
+			y = (m(0, 2) - m(2, 0)) * f;
+			z = (m(1, 0) - m(0, 1)) * f;
+		}
+		else if ((m00 > m11) && (m00 > m22))
+		{
+			x = sqrt(m00 - m11 - m22 + 1.0f) * 0.5f;
+			float f = 0.25f / x;
+			y = (m(1, 0) - m(0, 1)) * f;
+			z = (m(0, 2) - m(2, 0)) * f;
+			w = (m(2, 1) - m(1, 2)) * f;
+			
+		}
+		else if (m11 > m22)
+		{
+			y = sqrt(m11 - m00 - m22 + 1.0f) * 0.5f;
+			float f = 0.25f / y;
+			x = (m(1, 0) - m(0, 1)) * f;
+			z = (m(2, 1) - m(1, 2)) * f;
+			w = (m(0, 2) - m(2, 0)) * f;
+		}
+		else
+		{
+			z = sqrt(m22 - m00 - m11 + 1.0f) * 0.5f;
+			float f = 0.25f / z;
+			x = (m(0, 2) - m(2, 0)) * f;
+			y = (m(2, 1) - m(1, 2)) * f;
+			z = (m(1, 0) - m(0, 1)) * f;
+		}
+	}
 };
+
+Vector3D Transform(const Vector3D& v, const Quaternion& q)
+{
+	const Vector3D& b = q.GetVectorPart();
+	float bsquared = b.x * b.x + b.y * b.y + b.z * b.z;
+	return (v * (q.w * q.w - bsquared) + b * (Dot(v, b) * 2.0f) +
+		Cross(b, v) * (q.w * 2.0f));
+}
 
 Quaternion operator *(const Quaternion& q1, const Quaternion& q2)
 {
